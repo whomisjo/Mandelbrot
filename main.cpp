eabfbcdeabc012620ff97e2022ad0835c6ee09e4
whomisjo <130593047+whomisjo@users.noreply.github.com>
2023-04-26 11:00:55 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,44 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include "ComplexPlane.h"
 using namespace std;
 using namespace sf;
 
 int main()
 {
-    VideoMode::getDesktopMode().width;
-    VideoMode::getDesktopMode().height;
+ 
+    VideoMode vm(1920, 1080);
+    RenderWindow window(vm, "Mandelbrot", Style::Default);
+    sf::Event event;
 
     int aspect = VideoMode::getDesktopMode().height / VideoMode::getDesktopMode().width;
-        cout << aspect;
+    cout << aspect;
 
-
+    sf::Font font;										//title and directions
+    font.loadFromFile("fonts/KOMIKAP_.ttf");
     
+
+
+    ComplexPlane object;
+    while (window.isOpen())
+    {
+        window.display();
+
+
+
+        while (window.pollEvent(event))
+        {
+
+            switch (event.type)
+            {
+
+            case sf::Event::Closed:					//exit event
+                window.close();
+                break;
+
+            default:
+                break;
+            }
+        }
+    }
 }
