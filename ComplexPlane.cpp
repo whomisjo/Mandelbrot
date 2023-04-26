57bf5a750034f540b52a59443cc2fee2b3487585
IsaacJa75 <131226995+IsaacJa75@users.noreply.github.com>
2023-04-26 11:30:59 -0700

    Update ComplexPlane.cpp

---
+++ ComplexPlane.cpp
@@ -0,0 +1,49 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
+#include "ComplexPlane.h"
+
+
+ComplexPlane::ComplexPlane(float aspectRatio)
+{
+
+
+}
+
+
+void ComplexPlane::zoomIn()
+{
+
+
+
+}
+
+void ComplexPlane::zoomOut() 
+{
+
+}
+
+void ComplexPlane::setCenter(Vector2f coord) 
+{
+
+}
+
+void ComplexPlane::setMouseLocation(Vector2f coord)
+{
+
+}
+
+void ComplexPlane::loadText(Text& text)
+{
+
+}
+
+size_t ComplexPlane::countIterations(Vector2f coord)
+{
+
+}
+
+void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b)
+{
+
+}
