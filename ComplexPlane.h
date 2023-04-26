396811697b274d7eaf34ae7e3cded763b534049d
IsaacJa75 <131226995+IsaacJa75@users.noreply.github.com>
2023-04-26 11:31:28 -0700

    Update ComplexPlane.h

--- ComplexPlane.h
+++ ComplexPlane.h
@@ -3,5 +3,19 @@
 
 class ComplexPlane
 {
+public:
+	ComplexPlane(float aspectRatio);
+	void zoomIn();
+	void zoomOut();
+	void setCenter(Vector2f coord );
+	void setMouseLocation(Vector2f coord);
+	void loadText(Text& text);
+	size_t countIterations(Vector2f coord);
+	void iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b);
+private:
+	Vector2f m_mouseLocation;
+	View m_view;
+	int m_zoomCount;
+	float m_aspectRatio;
 
 };
