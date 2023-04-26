6c7e82bf08b444831522d2bc5407e1594d5177ea
whomisjo <130593047+whomisjo@users.noreply.github.com>
2023-04-26 11:25:22 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,39 +6,81 @@ using namespace sf;
 
 int main()
 {
- 
     VideoMode vm(1920, 1080);
     RenderWindow window(vm, "Mandelbrot", Style::Default);
-    sf::Event event;
+    const sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+    const float aspectRatio = static_cast<float>(desktop.height) / static_cast<float>(desktop.width);
+    cout << aspectRatio;
 
-    int aspect = VideoMode::getDesktopMode().height / VideoMode::getDesktopMode().width;
-    cout << aspect;
-
-    sf::Font font;										//title and directions
+    sf::Font font;										
     font.loadFromFile("fonts/KOMIKAP_.ttf");
+    sf::Text text;
+    text.setFont(font);
+    text.setCharacterSize(20);
+    text.setFillColor(sf::Color::White);
     
+    enum class State
+    {
+        CALCULATING,
+        DISPLAYING
+    };
+    State state = State::CALCULATING;
+ 
+    ComplexPlane complexPlane;
+    sf::VertexArray vArray(sf::Points, desktop.width* desktop.height);
 
-
-    ComplexPlane object;
     while (window.isOpen())
     {
         window.display();
-
-
-
+        sf::Event event;
         while (window.pollEvent(event))
         {
-
-            switch (event.type)
+            if (Keyboard::isKeyPressed(Keyboard::Escape)) window.close();
+            else if (event.type == sf::Event::MouseButtonPressed)
             {
+                const sf::Vector2f complexPlaneClick = complexPlane.mapPixelToCoords(sf::Vector2i(event.mouseButton.x, event.mouseButton.y), window);
 
-            case sf::Event::Closed:					//exit event
-                window.close();
-                break;
-
-            default:
-                break;
+                if (event.mouseButton.button == sf::Mouse::Left)
+                {
+                    complexPlane.zoomIn(complexPlaneClick);
+                }
+                else if (event.mouseButton.button == sf::Mouse::Right)
+                {
+                    complexPlane.zoomOut(complexPlaneClick);
+                }
+            }
+            else if (event.type == sf::Event::MouseMoved)
+            {
+                const sf::Vector2f complexPlaneMouse = complexPlane.mapPixelToCoords(sf::Vector2i(event.mouseMove.x, event.mouseMove.y), window);
+                complexPlane.setMouseLocation(complexPlaneMouse);
             }
         }
+
+        if (state == State::CALCULATING)
+        {
+            int pixelWidth = desktop.width;
+            int pixelHeight = desktop.height;
+            for (int i = 0; i < pixelHeight; i++)
+            {
+                for (int j = 0; j < pixelWidth; j++)
+                {
+                    vArray[j + i * pixelWidth].position = { (float)j,(float)i };
+
+                    sf::Vector2f coord = window.mapPixelToCoords(sf::Vector2i(j, i), complexPlane.getView());
+                    int iterations = complexPlane.countIterations(coord);
+                    Uint8 r, g, b;
+                    complexPlane.iterationsToRGB(iterations, r, g, b);
+
+                    vArray[j + i * pixelWidth].color = { r,g,b };
+                }
+            }
+            state = State::DISPLAYING;
+            complexPlane.loadText(text);
+        }
+
+        window.clear();
+        window.draw(vArray);
+        window.draw(text);
+        window.display();   
     }
 }
