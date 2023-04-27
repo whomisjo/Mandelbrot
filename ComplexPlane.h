169cd5c8ca3616ebf70feb2e83e4a6f856d620f3
whomisjo <130593047+whomisjo@users.noreply.github.com>
2023-04-27 15:01:53 -0700

    Update ComplexPlane.h

--- ComplexPlane.h
+++ ComplexPlane.h
@@ -1,8 +1,15 @@
 #pragma once
 
 
+using namespace sf;
+
+
 class ComplexPlane
 {
+	const unsigned int MAX_ITER = 64;
+	const float BASE_WIDTH = 4.0;
+	const float BASE_HEIGHT = 4.0;
+	const float BASE_ZOOM = 0.5;
 public:
 	ComplexPlane(float aspectRatio);
 	void zoomIn();
@@ -12,6 +19,7 @@ public:
 	void loadText(Text& text);
 	size_t countIterations(Vector2f coord);
 	void iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b);
+	View getView();
 private:
 	Vector2f m_mouseLocation;
 	View m_view;
